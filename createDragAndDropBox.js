0d4177bc88fbe8b3f0dbfc3941ddf35329f712be
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2020-09-20 14:52:06 -0400

    Add files via upload

--- createDragAndDropBox.js
+++ createDragAndDropBox.js
@@ -1,47 +1,45 @@
 (function() {
 
-	let body = document.getElementsByTagName('body')[0];
+  let body = document.getElementsByTagName('body')[0];
 
-	let container = document.createElement('div');
-	container.classList += "addedDropContainer outer";
-	container.style.border = "2px dashed rgb(170, 170, 170)";
-	container.style.backgroundColor = "rgba(255, 255, 255, 255)";
-	container.style.opacity = "1";
-	container.style.width = "350px";
-	container.style.height = "250px";
-	container.style.zIndex = "99";
-	container.style.margin = "auto";
-	container.style.position = "fixed";
-	container.style.display = "block";
-	container.style.top = "150px";
-	container.style.left = "50px";
-	container.style.display = 'table';
-	container.style.color = "#111";
+  let container = document.createElement('div');
+  container.classList += "addedDropContainer outer";
+  container.style.border = "2px dashed rgb(170, 170, 170)";
+  container.style.backgroundColor = "rgba(255, 255, 255, 255)";
+  container.style.opacity = "1";
+  container.style.width = "350px";
+  container.style.height = "250px";
+  container.style.zIndex = "99";
+  container.style.margin = "auto";
+  container.style.position = "fixed";
+  container.style.display = "block";
+  container.style.top = "150px";
+  container.style.left = "50px";
+  container.style.display = 'table';
+  container.style.color = "#111";
 
-	let middle = document.createElement('div');
-	middle.style.display = 'table-cell';
-	middle.style.verticalAlign = 'middle';
+  let middle = document.createElement('div');
+  middle.style.display = 'table-cell';
+  middle.style.verticalAlign = 'middle';
 
-	let inner = document.createElement('div');
+  let inner = document.createElement('div');
 
+  let p = document.createElement('p');
+  p.style.textAlign = "center";
+  p.style.fontFamily = "calibri";
+  p.style.fontSize = "20px";
+  p.innerText = "drag and drop a file here";
 
-	let p = document.createElement('p');
-	p.style.textAlign = "center";
-	p.style.fontFamily = "calibri";
-	p.style.fontSize = "20px";
-	p.innerText = "drag and drop a file here";
-
-	let s = document.createElement('div');
-	//s.innerText = "x";
-	s.id = 'closeDiv';
-	s.style.textAlign = "center";
-	s.style.verticalAlign = "center";
-	s.style.margin = '5px';
-	s.style.padding = '3px';
-	s.style.float = 'right';
-	s.style.fontFamily = "calibri";
-	s.style.fontSize = "20px";
-	s.style.border = '1px solid #ccc';
+  let s = document.createElement('div');
+  s.id = 'closeDiv';
+  s.style.textAlign = "center";
+  s.style.verticalAlign = "center";
+  s.style.margin = '5px';
+  s.style.padding = '3px';
+  s.style.float = 'right';
+  s.style.fontFamily = "calibri";
+  s.style.fontSize = "20px";
+  s.style.border = '1px solid #ccc';
 	// here it is:
 	s.style.position = 'absolute';
 	s.style.top = 0;
@@ -90,38 +88,23 @@
 		this.style.border = '2px dashed rgb(170, 170, 170)';
 	}, false);
 
-	container.addEventListener('drop', function(e) {
-		e.stopPropagation();
-		e.preventDefault();
-		this.style.border = '2px dashed rgb(170, 170, 170)';
+  container.addEventListener('drop', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.style.border = '2px dashed rgb(170, 170, 170)';
 
-		let file = e.dataTransfer.files[0];
+    let file = e.dataTransfer.files[0];
 		
-		let fileExtension = file.name.split('.')[file.name.split('.').length-1];
-		//console.log(fileExtension);
-
-		let isExcelFile = false;
-
-		if (fileExtension === 'xlsx') {
-			isExcelFile = true;
-		}		
+    let fileExtension = file.name.split('.')[file.name.split('.').length-1];
 		
-		/* 1. make a fileReader object */
-		var reader = new FileReader();
+    /* 1. make a fileReader object */
+    var reader = new FileReader();
 			
-		/* 2. we define what happens when the fileReader is done reading something */
-		reader.addEventListener('load', function(e) {
-			var data = e.target.result;
-			//console.log(fileContentAsText);
-			
-			if (isExcelFile) {
-				parseDataIfExcelFile(data);
-			} else {
-				console.log(data);
-			}
-
-		}, false);
-
+    /* 2. we define what happens when the fileReader is done reading something */
+    reader.addEventListener('load', function(e) {
+      var fileContents = e.target.result;
+      console.log(fileContents);
+    }, false);
 
     /* 3. use the fileReader object to read the file, and return a string */
     // in the case of xlsx file, we use readAsBinaryString as opposed to readAsText
